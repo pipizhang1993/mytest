151b3115107f7079f8f8588d49e4d8be705009bc
pipizhang1993 <zhy@@111>
2020-02-23 18:48:36 +0800

    11

---
+++ src/main/java/mytest/pipi/group/mytest/controller/MyFirstController.java
@@ -0,0 +1,15 @@
+package mytest.pipi.group.mytest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyFirstController {
+    @RequestMapping("/hello")
+    public String myhello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
