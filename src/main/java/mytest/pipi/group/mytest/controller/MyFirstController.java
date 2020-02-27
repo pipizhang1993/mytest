fe83f45c30957d33225ce648010960840bec712b
pipizhang1993 <zhy@@111>
2020-02-27 22:35:27 +0800

    分支提交

--- src/main/java/mytest/pipi/group/mytest/controller/MyFirstController.java
+++ src/main/java/mytest/pipi/group/mytest/controller/MyFirstController.java
@@ -10,7 +10,7 @@ public class MyFirstController {
     @RequestMapping("/hello")
     public String myhello(@RequestParam(name = "name") String name, Model model) {
         model.addAttribute("name", name);
-        //////
+        //////2132132
         return "hello";
     }
 }
